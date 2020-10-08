510e693b8d492e1b1b4695f41800008b9851b70d
milanpjevic <milanpjevic@gmail.com>
2020-10-08 19:29:36 +0200

    first commit

---
+++ src/components/UI/Modal/Modal.js
@@ -0,0 +1,35 @@
+import React, { Component } from "react";
+import classes from "./Modal.module.css";
+import Aux from "../../../hoc/ReactAux";
+import Backdrop from "../Backdrop/Backdrop";
+
+class Modal extends Component {
+  shouldComponentUpdate(nextProps, nextState) {
+    return nextProps.show !== this.props.show;
+  }
+
+  UNSAFE_() {
+    console.log("[Modal] WillUpdate");
+  }
+
+  render() {
+    return (
+      <Aux>
+        <Backdrop show={this.props.show} clicked={this.props.modalClosed} />
+        <div
+          className={classes.Modal}
+          style={{
+            transform: this.props.show
+              ? "translate(-50%, 0)"
+              : "translate(-50%, -100vh)",
+            opacity: this.props.show ? "1" : "0",
+          }}
+        >
+          {this.props.children}
+        </div>
+      </Aux>
+    );
+  }
+}
+
+export default Modal;
