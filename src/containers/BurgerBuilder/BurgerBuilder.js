a3dba49658c0785e874cc02231a930b9be4f340d
milanpjevic <milanpjevic@gmail.com>
2020-10-09 22:52:34 +0200

    Handling Errors

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -5,8 +5,8 @@ import BuildControls from "../../components/Burger/BuildControls/BuildControls";
 import Modal from "../../components/UI/Modal/Modal";
 import OrderSummary from "../../components/Burger/OrderSummary/OrderSummary";
 import axios from "../../axios-orders";
-import Spinner from "../../components/UI/Spinner/Spinner"
-import { findAllByDisplayValue } from "@testing-library/react";
+import Spinner from "../../components/UI/Spinner/Spinner";
+import withErrorHandler from "../../hoc/withErrorHandler/withErrorHandler";
 
 const INGREDIENT_PRICES = {
   salad: 0.5,
@@ -98,7 +98,7 @@ class BurgerBuilder extends Component {
         deliveryMethod: "fast"
     }
 
-    axios.post("/orders.json", order )
+    axios.post("/orders,json", order )
       .then(response => {
           this.setState({loading: false, purchasing: false})
       })
@@ -145,4 +145,4 @@ class BurgerBuilder extends Component {
   }
 }
 
-export default BurgerBuilder;
+export default withErrorHandler(BurgerBuilder, axios);
