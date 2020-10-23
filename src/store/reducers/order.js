084786b3e9c549ed212d12eed5c6f800d8a2ad41
milanpjevic <milanpjevic@gmail.com>
2020-10-23 12:55:53 +0200

    Order reducer

---
+++ src/store/reducers/order.js
@@ -0,0 +1,30 @@
+import * as actionType from "../actions/actionType";
+
+const initialState = {
+	orders: [],
+	loading: false,
+};
+
+const reducer = (state = initialState, action) => {
+	switch (actionType) {
+		case actionType.PURCHASE_BURGER_SUCCESS:
+			const newOrder = {
+				...action.orderData,
+				id: action.orderId,
+			};
+			return {
+				...state,
+				loading: false,
+				odreds: state.orders.concat(newOrder),
+			};
+		case actionType.PURCHASE_BURGER_FAIL:
+			return {
+				...state,
+				loading: false,
+			};
+		default:
+			return state;
+	}
+};
+
+export default reducer;
